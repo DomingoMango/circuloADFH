90dc310358c2ad35e611838a6fb98cdf815663da
DomingoMango <l21121601@morelia.tecnm.mx>
2022-04-02 16:49:35 -0600

    Circulo perfecto, si, obvio

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) {
         float Radio;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el radio");
+        System.out.println("Ingrese el Radio");
         Radio=sc.nextFloat();
         Circulo circulo = new Circulo();
         Circulo circulo1 = new Circulo(Radio,22f,3,2);
